a9f9a3630fedfbf509672340a2f62e94f304d56e
htbao1208 <kid03789125@gmail.com>
2021-05-09 15:02:03 +0700

    upload giao diện login

--- Hidebook/app/src/main/java/com/example/hidebook/MainActivity.java
+++ Hidebook/app/src/main/java/com/example/hidebook/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.hidebook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // hide the title bar
+        getSupportActionBar().hide();
+        //Set full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
